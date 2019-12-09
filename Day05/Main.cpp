dbb08dd6bf5d0138c5b671c8a2598d8db4df9430
iulianpopa1 <iulianip98@gmail.com>
2019-12-09 16:40:12 +0200

    Day 5 - Modularization & code refactoring

--- Day05/Main.cpp
+++ Day05/Main.cpp
@@ -10,6 +10,123 @@
 #include <stack>
 
 using namespace std;
+using ll = long long;
+
+void split_instructions(vector<ll> vec, ll index, ll& opcode, ll& C, ll& B, ll& A)
+{
+	ll coded = vec[index];
+
+	// mode
+	// A B C OP
+	// - - - --
+	opcode = coded % 100;
+	coded /= 100;
+	C = coded % 10;
+	coded /= 10;
+	B = coded % 10;
+	coded /= 10;
+	A = coded % 10;
+	coded /= 10;
+}
+
+void decode_instructions(vector<ll> vec, ll index, ll& C, ll& B, ll& A)
+{
+	switch (C)
+	{
+	case 0:
+		C = vec[index + 1];
+		break;
+	case 1:
+		C = index + 1;
+		break;
+	}
+
+	switch (B)
+	{
+	case 0:
+		B = vec[index + 2];
+		break;
+	case 1:
+		B = index + 2;
+		break;
+	}
+
+	switch (A)
+	{
+	case 0:
+		A = vec[index + 3];
+		break;
+	case 1:
+		A = index + 3;
+		break;
+	}
+}
+
+void execute_instructions(vector<ll> vec, ll INPUT_VALUE)
+{
+	ll index = 0;
+	while (1)
+	{
+		ll opcode, C, B, A;
+
+		split_instructions(vec, index, opcode, C, B, A);
+
+		if (opcode == 99)
+		{
+			break;
+		}
+
+		decode_instructions(vec, index, C, B, A);
+
+
+		switch (opcode)
+		{
+		case 1: // A = B + C
+			vec[A] = vec[B] + vec[C];
+			index += 4;
+			break;
+		case 2: // A = B * C
+			vec[A] = vec[B] * vec[C];
+			index += 4;
+			break;
+		case 3: // C = INPUT
+			vec[C] = INPUT_VALUE;
+			index += 2;
+			break;
+		case 4: // OUTPUT
+			if(vec[C])
+				cout << vec[C] << endl;
+			index += 2;
+			break;
+		case 5: // jump-if-true
+			if (vec[C] != 0)
+				index = vec[B];
+			else
+				index += 3;
+			break;
+		case 6: // jump-if-false
+			if (vec[C] == 0)
+				index = vec[B];
+			else
+				index += 3;
+			break;
+		case 7: // A = (C < B)
+			if (vec[C] < vec[B])
+				vec[A] = 1;
+			else
+				vec[A] = 0;
+			index += 4;
+			break;
+		case 8: // A = (C == B)
+			if (vec[C] == vec[B])
+				vec[A] = 1;
+			else
+				vec[A] = 0;
+			index += 4;
+			break;
+		}
+	}
+}
 
 ifstream in("P_input.txt");
 // parameter modes - single digits
@@ -20,9 +137,9 @@ int main()
 {
 	ios::sync_with_stdio(false);
 
-	vector <int> vec;
+	vector <ll> vec;
 
-	int x, index;
+	ll x;
 
 	while (!in.eof())
 	{
@@ -30,139 +147,12 @@ int main()
 		vec.push_back(x);
 	}
 
-	index = 0;
 
+	cout << "PART 1 : ";
+	ll INPUT_VALUE = 1; /// HERE /// PART1 - 1, PART2 - 5
+	execute_instructions(vec, INPUT_VALUE);	
 
-	int INPUT_VALUE = 5; /// HERE ///
-
-	while (1)
-	{
-		int val = vec[index];
-		int opcode = val % 100;
-		val /= 100;
-		int modeC = val % 10;
-		val /= 10;
-		int modeB = val % 10;
-		val /= 10;
-		int modeA = val % 10;
-		val /= 10;
-
-		int opB = -1, opC = -1;
-
-		if (opcode == 1)//sum
-		{
-			if (modeB == 0)
-				opB = vec[vec[index + 2]];
-			else			
-				opB = vec[index + 2];
-
-			if (modeC == 0) 
-				opC = vec[vec[index + 1]];
-			else			
-				opC = vec[index + 1];
-
-			vec[vec[index + 3]] = opB + opC;
-			index += 4;
-		}
-		else if (opcode == 2) //multiply
-		{
-			if (modeB == 0) 
-				opB = vec[vec[index + 2]];
-			else			
-				opB = vec[index + 2];
-
-			if (modeC == 0) opC = vec[vec[index + 1]];
-			else			opC = vec[index + 1];
-
-			if (modeC == 0)
-				opC = vec[vec[index + 1]];
-			else
-				opC = vec[index + 1];
-			vec[vec[index + 3]] = opB * opC;
-
-			index += 4;
-		}
-		else if (opcode == 3) //input
-		{
-			vec[vec[index + 1]] = INPUT_VALUE;
-			index += 2;
-		}
-		else if (opcode == 4) //output
-		{
-			cout << vec[vec[index + 1]] << endl;
-			index += 2;
-		}		
-		else if (opcode == 5) // jump-if-true
-		{			
-			if (modeB == 0)
-				opB = vec[vec[index + 2]];
-			else
-				opB = vec[index + 2];
-			if (modeC == 0)
-				opC = vec[vec[index + 1]];
-			else
-				opC = vec[index + 1];
-
-			if (opC != 0)
-				index = opB;
-			else
-				index += 3;
-		}
-		else if (opcode == 6) // jump-if-false
-		{
-			if (modeB == 0)
-				opB = vec[vec[index + 2]];
-			else
-				opB = vec[index + 2];
-			if (modeC == 0)
-				opC = vec[vec[index + 1]];
-			else
-				opC = vec[index + 1];
-
-			if (opC == 0)
-				index = opB;
-			else
-				index += 3;
-		}		
-		else if (opcode == 7)
-		{
-			if (modeB == 0)
-				opB = vec[vec[index + 2]];
-			else
-				opB = vec[index + 2];
-			if (modeC == 0)
-				opC = vec[vec[index + 1]];
-			else
-				opC = vec[index + 1];
-
-			if (opC < opB)
-				vec[vec[index + 3]] = 1;
-			else
-				vec[vec[index + 3]] = 0;
-			index += 4;
-		}		
-		else if (opcode == 8)
-		{
-			if (modeB == 0)
-				opB = vec[vec[index + 2]];
-			else
-				opB = vec[index + 2];
-			if (modeC == 0)
-				opC = vec[vec[index + 1]];
-			else
-				opC = vec[index + 1];
-
-			if (opC == opB)
-				vec[vec[index + 3]] = 1;
-			else
-				vec[vec[index + 3]] = 0;
-
-			index += 4;
-		}
-		else if (opcode == 99)
-		{
-			break;
-		}
-	}
-	
+	cout << "PART 2 : ";
+	INPUT_VALUE = 5; /// HERE /// PART1 - 1, PART2 - 5
+	execute_instructions(vec, INPUT_VALUE);
 }
