bb49a85ba5e3cc204ae6f6c612b5ba62cae3d66a
Iulian Popa <iulianip98@gmail.com>
2019-12-16 10:52:47 +0200

    Day16

--- Day16/Main.cpp
+++ Day16/Main.cpp
@@ -27,26 +27,55 @@ string phase(string x)
 		for (ll j = 0; j < x.size(); ++j)
 		{
 			local_ans += (x[j] - '0') * pat[pattern_it / (repeats)];
-			++pattern_it;
-			if (pattern_it >= (repeats) * 4)
-				pattern_it = 0;
+			pattern_it = (pattern_it + 1) % (repeats * 4);
 		}
-		new_x += to_string(abs(local_ans) % 10);
+		new_x += '0' + abs(local_ans) % 10;
 	}
 	return new_x;
+}
+
+string phase2(string x)
+{
+	vector<int> sums{ 0 };
+
+	for (auto it : x)
+	{
+		sums.push_back(it - '0' + sums.back());
+	}
+
+	for (int j = x.size() / 2; j < x.size(); ++j)
+	{
+		x[j] = (sums[x.size()] - sums[j]) % 10 + '0';
+	}
+
+	return x;
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
 
-	string x;
-	in >> x;
+	string x, input_x, BIG_X;
+	in >> input_x;
+
+	x = input_x;
 	for (int i = 1; i <= 100; ++i)
 	{
 		x = phase(x);
 	}
 	cout << "Part1: " << x.substr(0, 8) << endl;
+	
+	for (int i = 1; i <= 10000; ++i)
+	{
+		BIG_X += input_x;
+	}
 
+	for (int i = 1; i <= 100; ++i)
+	{
+		BIG_X = phase2(BIG_X);
+	}
+	
+	ll offset = stoll(input_x.substr(0, 7));
+	cout << "Part2: " << BIG_X.substr(offset, 8) << endl;
 	return 0;
 }
