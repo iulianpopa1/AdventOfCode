21efec24466c28fc7d851e76ff69a7050273dedb
Iulian Popa <iulian.popa@caphyon.com>
2020-12-09 19:10:53 +0200

    Day 9 - cleaned

--- code/Day9.py
+++ code/Day9.py
@@ -1,39 +1,51 @@
+from functools import partial
+import itertools
 import numpy as np
 from utils import DataFiles
 
 dataFiles = DataFiles(__file__)
 
-input = dataFiles.get_input()  # inputRaw = dataFiles.input
-inputEX = dataFiles.get_inputEX()  # inputRawEX = dataFiles.inputEX
-inputRaw = dataFiles.input
-inputRawEX = dataFiles.inputEX
+input = dataFiles.get_input()
 
-numbers = inputEX
+
+def part2_itertools(missing_number): # 2x faster than the Numpy method due **partial_sum** creation
+    for i in range(len(numbers)):
+        partial_sum = list(itertools.accumulate(numbers[i:]))[2:] # Faster
+        try:
+            if missing_number in partial_sum:
+                found_ps_index = partial_sum.index(missing_number)
+                sum_arr = numbers[i: i + 1 + found_ps_index + 2]
+                print('Part 2:', min(sum_arr) + max(sum_arr))
+        except:
+            pass
+
+
+def part2_numpy(missing_number):
+    for i in range(len(numbers)):
+        partial_sum = np.array(numbers[i:]).cumsum()[2:] # Slower
+        try:
+            if missing_number in partial_sum:
+                found_ps_index = np.where(partial_sum == missing_number)[0][0]
+                sum_arr = numbers[i: i + 1 + found_ps_index + 2]
+                print('Part 2:', min(sum_arr) + max(sum_arr))
+        except:
+            pass
+
 
 numbers = [int(line) for line in input]
 preamble = 25
-p1 = 0
-p2 = 0
+missing_number = 0
 
-for i, nr_i in enumerate(numbers[preamble:]):
+for i in range(preamble, len(numbers)):
     sums = set()
 
-    for j, nr_j in enumerate(numbers[i: i + preamble]):
-        for k, nr_k in enumerate(numbers[j + 1: i + preamble]):
+    for nr_j in numbers[i - preamble: i]:
+        for nr_k in numbers[i - preamble: i]:
             sums.add(nr_j + nr_k)
 
-    if nr_i not in sums:
-        p1 = nr_i
+    if numbers[i] not in sums:
+        missing_number = numbers[i]
 
-print('Part 1:', p1)
-
-for i in range(len(numbers)):
-    for j in range(i + 2, len(numbers)):
-        aux = numbers[i: j]
-
-        if sum(aux) == p1:
-            p2 = min(aux) + max(aux)
-
-print('Part 2:', p2)
-
-# TODO: use numpy.array
+print('Part 1:', missing_number)
+part2_itertools(missing_number)
+part2_numpy(missing_number)
