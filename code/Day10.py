17b812000d9f733e7d9062e69f810f8664b280dc
Iulian Popa <iulian.popa@caphyon.com>
2020-12-10 07:57:57 +0200

    Day 10 - refactor

--- code/Day10.py
+++ code/Day10.py
@@ -15,18 +15,13 @@ prev = 0
 
 for curr in numbers:
     diff = curr - prev
+    if diff == 1:
+        diff_1 += 1
     if diff == 3:
         diff_3 += 1
-    elif diff == 1:
-        diff_1 += 1
     prev = curr
 
-print('1s:', diff_1, ' | 3s:', diff_3, ' | ', diff_1 * diff_3)
-
-if numbers[0] == 1:
-    diff_1 += 1
-elif numbers[0] == 3:
-    diff_3 += 1
+print('Part 1:', diff_1 * diff_3)
 
 numbers.insert(0, 0)
 
@@ -37,4 +32,4 @@ for i in range(len(numbers)):
         if(numbers[i] - numbers[j] <= 3):
             dp[i] += dp[j]
 
-print(dp[len(numbers) - 1])
+print('Part 2:', dp[len(numbers) - 1])
