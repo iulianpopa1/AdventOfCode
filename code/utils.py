438014aba9b9cd2cb8751d014d7dc4fe397e49ba
Iulian Popa <iulian.popa@caphyon.com>
2020-12-17 08:22:36 +0200

    Update utils module

--- code/utils.py
+++ code/utils.py
@@ -1,21 +1,21 @@
 import re
 
 class DataFiles():
-    __code_path = ''
-    __input_path = ''
-    __example_path = ''
+    code_path = ''
+    input_path = ''
+    example_path = ''
     
     def __init__(self, path):
-        self.__code_path = path
-        self.__input_path = path.replace('code', 'inputs').replace('Day', '').replace('.py', '.txt')
-        self.__example_path = re.sub(r'(\w+)(.txt$)', r'Example\g<2>', self.__input_path)
-        self.input = open(self.__input_path)
-        self.inputEX = open(self.__example_path)
+        self.code_path = path
+        self.input_path = path.replace('code', 'inputs').replace('Day', '').replace('.py', '.txt')
+        self.example_path = re.sub(r'(\w+)(.txt$)', r'Example\g<2>', self.input_path)
+        self.input = open(self.input_path)
+        self.inputEX = open(self.example_path)
 
     def print_paths(self):
-        print(self.__code_path)
-        print(self.__input_path)
-        print(self.__example_path)
+        print(self.code_path)
+        print(self.input_path)
+        print(self.example_path)
 
     def get_input(self):
         return self.input.read().splitlines()
