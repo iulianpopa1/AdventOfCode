6f9b6609ad27750a76ecd35693784d028cd64ddb
Iulian Popa <iulianip98@gmail.com>
2021-12-06 07:30:27 +0200

    [update]: Day 6

--- code/Day06.py
+++ code/Day06.py
@@ -8,41 +8,27 @@ input = dataFiles.input
 inputEX = dataFiles.inputEX
 
 inp = input[:]
-
-print(inp)
-
 inpp = [int(x) for x in inp[0].split(",")]
 
-for day in range(80):
-    to_add = 0
-    for i in range(len(inpp)):
-        if inpp[i] == 0:
-            inpp[i] = 6
-            to_add += 1
-        else:
-            inpp[i] -= 1
 
-    for _ in range(to_add):
-        inpp.append(8)
+def simulate(values, days):
+    fishes = defaultdict(int)
+    for x in values:
+        fishes[x] += 1
+
+    for _ in range(days):
+        aux = defaultdict(int)
+        for k, v in fishes.items():
+            if k == 0:
+                aux[6] += v
+                aux[8] += v
+            else:
+                aux[k - 1] += v
+
+        fishes = aux
+
+    return sum(fishes.values())
 
 
-print(len(inpp))
-
-
-fish = defaultdict(int)
-for x in inpp:
-    fish[x] += 1
-
-for day in range(256):
-    aux = defaultdict(int)
-    for k, v in fish.items():
-        if k == 0:
-            aux[6] += v
-            aux[8] += v
-        else:
-            aux[k - 1] += v
-
-    fish = aux
-    # print(counts.values())
-
-print(sum(fish.values()))
+print("Part 1:", simulate(inpp, 80))
+print("Part 2:", simulate(inpp, 256))
