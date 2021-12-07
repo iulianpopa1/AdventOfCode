b4ea34d09733018d628fc91be10d9e2e2dea84b6
Iulian Popa <iulianip98@gmail.com>
2021-12-07 09:16:16 +0200

    [update]: Day template

--- code/DayExample.py
+++ code/DayExample.py
@@ -1,10 +1,11 @@
 from utils import DataFiles
 from collections import *
 from numpy import *
+from math import *
 
 dataFiles = DataFiles(__file__)
 
 input = dataFiles.input
 inputEX = dataFiles.inputEX
 
-inp = input[:]
+inp = inputEX[:]
