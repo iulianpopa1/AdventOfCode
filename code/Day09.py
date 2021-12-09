4f1a04d7931358551d11ccdc6d53e1bc0c2e7a39
Iulian Popa <iulianip98@gmail.com>
2021-12-09 11:06:44 +0200

    [update]: Day 09

--- code/Day09.py
+++ code/Day09.py
@@ -1,7 +1,7 @@
 from utils import DataFiles
 from collections import *
-from numpy import *
 from math import *
+import numpy as np
 
 dataFiles = DataFiles(__file__)
 
@@ -10,53 +10,66 @@ inputEX = dataFiles.inputEX
 
 inp = input[:]
 
-mat = [[9 for _ in range(len(inp) + 2)] for y in range(len(inp[0]) + 2)]
 
-for i in range(len(inp)):
-    for j in range(len(inp[0])):
-        mat[i + 1][j + 1] = int(inp[i][j])
+def get_neigh(x, y):
+    neigh = []
+
+    if x > 0:
+        neigh.append((x - 1, y, mat[x - 1][y]))
+    if x < len(mat) - 1:
+        neigh.append((x + 1, y, mat[x + 1][y]))
+    if y > 0:
+        neigh.append((x, y - 1, mat[x][y - 1]))
+    if y < len(mat[x]) - 1:
+        neigh.append((x, y + 1, mat[x][y + 1]))
+
+    return neigh
 
 
-def sc_mn(mat, x, y):
-    if (
-        mat[x][y] < mat[x + 1][y]
-        and mat[x][y] < mat[x - 1][y]
-        and mat[x][y] < mat[x][y + 1]
-        and mat[x][y] < mat[x][y - 1]
-    ):
+def is_low_point(x, y):
+    if all(mat[x][y] < z for _, _, z in get_neigh(x, y)):
         return mat[x][y]
     else:
         return -1
 
 
+rows = len(inp)
+cols = len(inp[0])
+mat = np.zeros((rows, cols), dtype=int)
+
+for x in range(len(inp)):
+    for y in range(len(inp[0])):
+        mat[x][y] = inp[x][y]
+
 ans = 0
-for x in range(1, len(inp) + 1):
-    for y in range(1, len(inp[0]) + 1):
-        ans += 1 + sc_mn(mat, x, y)
+for x in range(len(mat)):
+    for y in range(len(mat[0])):
+        ans += 1 + is_low_point(x, y)
 
 print("Part1:", ans)
 
-# part 2
-basins = []
-
 
 def cnt_b(x, y, start=False):
-    if mat[x][y] != 9 and mat[x][y] != -1:  # or y < 0 or y >= len(inp) or x < 0 or x >= len(inp[0])
+
+    if mat[x][y] != 9 and mat[x][y] != -1:
+        neigh = get_neigh(x, y)
+
         if start:
             basins.append(0)
         else:
             basins[len(basins) - 1] += 1
             mat[x][y] = -1
 
-        cnt_b(x + 1, y)
-        cnt_b(x - 1, y)
-        cnt_b(x, y + 1)
-        cnt_b(x, y - 1)
+        for nx, ny, _ in neigh:
+            cnt_b(nx, ny)
 
 
-for x in range(1, len(inp) + 1):
-    for y in range(1, len(inp[0]) + 1):
+basins = []
+
+for x in range(len(inp)):
+    for y in range(len(inp[0])):
         cnt_b(x, y, True)
 
 basins.sort(reverse=True)
+
 print("Part2:", basins[0] * basins[1] * basins[2])
