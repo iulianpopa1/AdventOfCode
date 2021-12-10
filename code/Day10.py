185eff2a3474de5af2d3be6753d8a443dd007326
Iulian Popa <iulianip98@gmail.com>
2021-12-10 13:24:47 +0200

    [add]: Refactor & clean Day 10

--- code/Day10.py
+++ code/Day10.py
@@ -1,7 +1,8 @@
 from utils import DataFiles
 from collections import *
-from numpy import *
 from math import *
+
+import numpy as np
 import re
 
 dataFiles = DataFiles(__file__)
@@ -11,52 +12,72 @@ inputEX = dataFiles.inputEX
 
 inp = input[:]
 
-incomplete_inp = []
+open_br = "({[<"
+close_br = ")}]>"
+
+ch_score_p1 = {"": 0, ")": 3, "]": 57, "}": 1197, ">": 25137}
+ch_score_p2 = {"": 0, "(": 1, "[": 2, "{": 3, "<": 4}
+
+map_br = dict(zip(open_br, close_br))
+
+line_scores = []
 
 
-def check2(my_string):
-    brackets = ["()", "{" + "}", "[]", "<>"]
-    open_tup = set("({[<")
-    found = True
-    while found:
-        found = False
-        for br in brackets:
-            fnd = my_string.find(br)
-            if fnd != -1:
-                found = True
-                my_string = my_string.replace(br, "")
+def calc_score_part2(my_string):
+    ans = 0
+    for c in reversed(my_string):
+        ans = ans * 5 + ch_score_p2[c]
 
-    if my_string != "" and set(my_string).issubset(open_tup):
-        return my_string
+    return ans
+
+
+def get_first_incorrect(line):
+    stack = []
+
+    # Part 1
+    for ch in line:
+        if ch in open_br:
+            stack.append(ch)
+        elif ch in close_br:
+            if stack == [] or ch != map_br[stack.pop()]:
+                return ch
+
+    # Part 2
+    if all(ch in open_br for ch in stack):
+        line_scores.append(calc_score_part2(stack))
 
     return ""
 
 
-def calc_s(my_string):
-    ans = 0
-
-    for c in reversed(my_string):
-        if c == "(":
-            ans = ans * 5 + 1
-        elif c == "[":
-            ans = ans * 5 + 2
-        elif c == "{":
-            ans = ans * 5 + 3
-        elif c == "<":
-            ans = ans * 5 + 4
-    print(ans)
-    return ans
+print("Part1:", sum(ch_score_p1[get_first_incorrect(line)] for line in inp))
+line_scores.sort()
+print("Part2:", line_scores[len(line_scores) // 2])
 
 
-ans = []
+### Another approach for PART 2
+incomplete_lines = {}
+
+
+def is_incomplete(line):
+    brackets = ["".join(br) for br in zip(open_br, close_br)]
+
+    while any(br in line for br in brackets):
+        for br in brackets:
+            line = line.replace(br, "")
+
+    if line != "" and set(line).issubset(open_br):
+        return line
+
+    return ""
+
 
 for string in inp:
-    kek = check2(string)
-    if kek != "":
-        print(string, "-", kek)
-        ans.append(int(calc_s(kek)))
+    aux = is_incomplete(string)
+    if aux != "":
+        incomplete_lines[string] = aux
 
-ans = sorted(ans)
-print(ans)
 
-print(ans[len(ans) // 2])
+ans = [calc_score_part2(v) for _, v in incomplete_lines.items()]
+ans.sort()
+
+print("Part2:", ans[len(ans) // 2])
