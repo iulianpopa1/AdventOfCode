980d274ef1825e8442ef9d8a3210f514e3607483
Iulian Popa <iulianip98@gmail.com>
2021-12-21 10:29:09 +0200

    [update]: Day 15

--- code/Day15.py
+++ code/Day15.py
@@ -16,6 +16,21 @@ inputEX = dataFiles.inputEX
 inp = input[:]
 
 
+def get_neigh_x(x, y, x_min=0, y_min=0, x_max=inf, y_max=inf, diag=False):
+    possible_x = [x]
+    possible_y = [y]
+
+    possible_x.append(x - 1) if x > x_min else None
+    possible_y.append(y - 1) if y > y_min else None
+    possible_x.append(x + 1) if x < x_max else None
+    possible_y.append(y + 1) if y < y_max else None
+
+    if diag == False:
+        return [(a, b) for a in possible_x for b in possible_y if (a, b) != (x, y) and (a != x ^ b != y)]
+    else:
+        return [(a, b) for a in possible_x for b in possible_y if (a, b) != (x, y)]
+
+
 def get_neigh(x, y):
     neigh = []
 
@@ -54,7 +69,7 @@ for x in range(len(inp)):
         mat[x][y] = int(inp[x][y])
 
 
-mat = repeatGrid(mat, 5)
+# mat = repeatGrid(mat, 5)
 
 for x in range(len(mat)):
     for y in range(len(mat[0])):
@@ -63,6 +78,10 @@ for x in range(len(mat)):
 for x in range(len(mat)):
     for y in range(len(mat[0])):
         ngh = get_neigh(x, y)
+        print(sorted(ngh))
+        ngh2 = get_neigh_x(x, y, x_max=len(mat) - 1, y_max=len(mat[0]) - 1)
+        print(sorted(ngh2))
+        print()
         for ng_x, ng_y in ngh:
             G.add_edge((x, y), (ng_x, ng_y), weight=mat[ng_x][ng_y])
             G.add_edge((ng_x, ng_y), (x, y), weight=mat[x][y])
