b14979d29dba0405f32ca106599b13fa87f6635d
Iulian Popa <iulianip98@gmail.com>
2022-12-02 14:27:41 +0200

    Add Day 1

---
+++ code/Day01.py
@@ -0,0 +1,20 @@
+from utils import DataFiles
+
+dataFiles = DataFiles(__file__)
+
+input = dataFiles.get_input_chunks(sep="\n\n")
+inputEX = dataFiles.get_input(splitlines=True, example=True)
+
+all_calories = []
+for chunk in input:
+    total_calories = 0
+
+    for line in chunk.splitlines():
+        total_calories += int(line)
+
+    all_calories.append(total_calories)
+
+all_calories.sort(reverse=True)
+
+print("Part 1:", all_calories[0])
+print("Part 2:", all_calories[:3], "=>", sum(all_calories[:3]))
