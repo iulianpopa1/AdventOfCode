4d8b9a670083147bbe9146c4cd4067720e52b2ae
Iulian Popa <iulianip98@gmail.com>
2022-12-03 23:07:46 +0200

    Update Day 03

--- code/Day03.py
+++ code/Day03.py
@@ -6,12 +6,22 @@ dataFiles = DataFiles(__file__)
 input = dataFiles.get_input(splitlines=True)  # dataFiles.get_input_chunks(sep="\n\n")
 inputEX = dataFiles.get_input(splitlines=True, example=True)
 
+
+def find_common_letters(items):
+    to_return = set(items[0])
+
+    for item in items:
+        to_return &= set(item)
+
+    return to_return
+
+
 p1 = 0
 
 for rucksack in input:
-    ruk_1, ruk_2 = rucksack[: len(rucksack) // 2], rucksack[len(rucksack) // 2 :]
+    half_splits = [rucksack[: len(rucksack) // 2], rucksack[len(rucksack) // 2 :]]
 
-    common = list(set(ruk_1) & set(ruk_2))
+    common = list(find_common_letters(half_splits))
 
     if common[0].islower():
         p1 += ord(common[0]) - 97 + 1
@@ -24,9 +34,9 @@ print("Part1:", p1)
 p2 = 0
 
 # for pos in range(len(input) // 3):
-#     ruk_1, ruk_2, ruk_3 = input[pos * 3], input[pos * 3 + 1], input[pos * 3 + 2]
+#   common = list(find_common_letters(input[pos * 3 : pos * 3 + 2]))
 for chunk_of_3 in np.reshape(input, (len(input) // 3, 3)):
-    common = list(set(chunk_of_3[0]) & set(chunk_of_3[1]) & set(chunk_of_3[2]))
+    common = list(find_common_letters(chunk_of_3))
 
     if common[0].islower():
         p2 += ord(common[0]) - 96
