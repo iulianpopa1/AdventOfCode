3dc62374411fce39ffd49a7d322f807f4dfee514
Iulian Popa <iulianip98@gmail.com>
2022-12-04 07:39:33 +0200

    Modify Day 4

--- code/Day04.py
+++ code/Day04.py
@@ -7,21 +7,22 @@ inputEX = dataFiles.get_input(splitlines=True, example=True)
 
 
 def range_overlap_p1(x1, x2, y1, y2):
-    return int(x1) <= int(y1) <= int(y2) <= int(x2) or int(y1) <= int(x1) <= int(x2) <= int(y2)
+    return x1 <= y1 <= y2 <= x2 or y1 <= x1 <= x2 <= y2
 
 
 def range_overlap_p2(x1, x2, y1, y2):
-    return (int(x2) >= int(y1) and int(x1) <= int(y2)) or (int(y2) >= int(x1) and int(y1) <= int(x2))
+    return (x2 >= y1 and x1 <= y2) or (y2 >= x1 and y1 <= x2)
 
 
-ttl_p1 = 0
-ttl_p2 = 0
+total_p1 = 0
+total_p2 = 0
+
 for line in input:
     x, y = line.split(",")
-    x1, x2 = x.split("-")
-    y1, y2 = y.split("-")
-    ttl_p1 += range_overlap_p1(x1, x2, y1, y2)
-    ttl_p2 += range_overlap_p2(x1, x2, y1, y2)
+    x1, x2 = map(int, x.split("-"))
+    y1, y2 = map(int, y.split("-"))
+    total_p1 += range_overlap_p1(x1, x2, y1, y2)
+    total_p2 += range_overlap_p2(x1, x2, y1, y2)
 
-print(ttl_p1)
-print(ttl_p2)
+print("Part1:", total_p1)
+print("Part2:", total_p2)
