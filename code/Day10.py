53ba7cf8400703b2c7ea8263dc283dc2d09caa67
Iulian Popa <iulianip98@gmail.com>
2022-12-10 07:40:30 +0200

    Add Day 10

---
+++ code/Day10.py
@@ -0,0 +1,44 @@
+from utils import DataFiles
+
+dataFiles = DataFiles(__file__)
+
+input = dataFiles.get_input(splitlines=True)  # dataFiles.get_input_chunks(sep="\n\n")
+inputEX = dataFiles.get_input(splitlines=True, example=True)
+
+
+instr_queue = []
+for line in input:
+    instr_queue.append(0)
+
+    aux = line.split()
+    if aux[0] == "addx":
+        instr_queue.append(int(aux[1]))
+
+
+x = 1
+signals_strength_sum = 0
+
+for cycle in range(len(instr_queue)):
+    cycle_val = instr_queue[cycle]
+
+    if cycle % 40 == 19:
+        signals_strength_sum += (cycle + 1) * x
+
+    x += cycle_val
+
+print(signals_strength_sum)
+
+
+x = 1
+for cycle in range(len(instr_queue)):
+    cycle_val = instr_queue[cycle]
+
+    if cycle % 40 in [x - 1, x, x + 1]:
+        print("#", end="")
+    else:
+        print(" ", end="")
+
+    if cycle % 40 == 39:
+        print()
+
+    x += cycle_val
